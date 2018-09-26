e4d647f8c0d1fa38862f680510c14325452db290
UlugbekYusupov <ulugbek19960902@gmail.com>
2018-09-27 04:29:32 +0900

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -6,23 +6,33 @@ int main() {
 	STACK* stack2 = createStack();
 	bool p = true;
 	while (p) {
-		int valueIN;
-		scanf_s("%d", &valueIN);
-		if (valueIN == 0) {
-			p = false;
+		char ch;
+		cin >> ch;
+		switch (ch)
+		{
+		case 'i':
+			int valueIN;
+			scanf_s("%d", &valueIN);
+			pushStack(stack1, valueIN);
+			break;
+		case 't':
+			while (!emptyStack(stack1)) {
+				int valueIN = popStack(stack1);
+				pushStack(stack2, valueIN);
+			}
+			break;
+		case 'p':
+			while (!emptyStack(stack2)) {
+				int data = popStack(stack2);
+				cout << data << " ";
+			}
+			break;
+		case 'e':
+			exit(0);
 			break;
 		}
-		pushStack(stack1, valueIN);
-	}
-	while (!emptyStack(stack1)) {
-		int valueIN = popStack(stack1);
-		pushStack(stack2, valueIN);
 	}
 	destroyStack(stack1);
-	while (!emptyStack(stack2)) {
-		int data = popStack(stack2);
-		cout << data << " ";
-	}
 	destroyStack(stack2);
 	_getch();
 	return 0;
